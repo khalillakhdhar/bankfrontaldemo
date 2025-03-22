53b96dded691ff4283db9d99fdcb5c66c41060ba
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2025-03-22 11:58:19 +0100

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  credentials={userName:'',password:''};
+  constructor(private authService:AuthService,private router:Router){}
+  onSubmit()
+  {
+    this.authService.login(this.credentials).subscribe(
+    (response)=>{
+      this.authService.saveToken(response.token);
+      this.authService.saveUserRoles(response.roles);
+     const roles=this.authService.getUserRoles();
+      if(roles.includes('ADMIN_ROLES'))
+      {
+        this.router.navigate(['/admin']);
+      }
+      else if(roles.includes('USER_ROLES'))
+      {
+        this.router.navigate(['/client']);
+      }
+      else
+      {
+        this.router.navigate(['/auth']);
+      }
+    }
+    )
+  }
 
 }
